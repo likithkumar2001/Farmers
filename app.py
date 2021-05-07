3c8911bd4334f778622433e8ee30febd4b17bff3
likithkumar2001 <83726218+likithkumar2001@users.noreply.github.com>
2021-05-07 18:09:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,8 +24,6 @@ st.write(" Message : ", x['Message'])
 st.write(" Date of issue : ", x['Date of issue'])
 if (x['Time of issue'] < 12):
     st.write(" Time of issue : ", str(x['Time of issue'])+" AM")
-elif(x['Time of issue '] >= 12 and x['Time of issue '] < 13  ):
-    st.write(" Time of issue  : ", str(x['Time of issue']) + " PM")
 else:
     st.write(" Time of issue : ", str(round(x['Time of issue']-12,2)) + " PM")
 if (x['Valid upto'] < 12):
