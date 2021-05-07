0aa52948d9c3fc48e75332461f2a932583a920fa
likithkumar2001 <83726218+likithkumar2001@users.noreply.github.com>
2021-05-07 17:21:19 +0530

    Add files via upload

---
+++ data_accumulation.py
@@ -0,0 +1,67 @@
+
+def web_scrapping():
+    import requests
+    from bs4 import BeautifulSoup
+    import re
+    url = "https://mausam.imd.gov.in/imd_latest/contents/districtwisewarnings.php"
+    html = requests.get(url)
+    soup_day = BeautifulSoup(html.content, 'html.parser')
+    data =str(soup_day.find_all('script')[7])
+    return(data)
+
+def data_cleaning(data):
+    import json
+    from bs4 import BeautifulSoup
+    import re
+    data=re.sub(r"^\s+|\s+$", "", data)
+    data = data.replace("\n","")
+    text = '\[(.*?)\]'
+    data = "["+ re.findall(text,data)[0] +"]"
+    data_json = json.loads(data)
+    for i in range(len(data_json)):
+        data_json[i]['info']=data_json[i]['info'].replace("</br>"," ")
+        data_json[i].pop('color', None)
+        data_json[i].pop('balloonText', None)
+        data_json[i]['id']=int(data_json[i]['id'])
+        y = data_json[i]['info']
+        y = BeautifulSoup(y, 'html.parser')
+        data_json[i]['info'] = y.text
+        p = data_json[i]['info'].split('Time of issue:')
+        x= p[0].lstrip()
+        data_json[i]['Message'] = x.rstrip()
+        data_json[i]['Type'] = data_json[i]['Message'].split(':')[0]
+        if(len(p)>1):
+            time = p[1].split('Valid upto:')
+            x = time[0].rstrip()
+            x = x.lstrip()
+            x=x.split(" ")
+            data_json[i]['Date of issue'] = x[0]
+            data_json[i]['Time of issue'] = int(x[1])/100
+            x = time[1].rstrip()
+            x = x.lstrip()
+            x = x.split("Hrs")
+            data_json[i]['Valid upto'] = int(x[0])/100
+        data_json[i].pop('info', None)
+    return(data_json)
+
+
+def store_data(info):
+    import urllib
+    from pymongo import MongoClient
+    import dns
+    uri = 'mongodb+srv://likith:'+  urllib.parse.quote("Rp-iA@c6!Nq45c4") +'@cluster0.ms0ap.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    client = MongoClient( uri )
+    db = client.weather
+    data = db.data
+    result = data.insert_many(info)
+    result
+
+    '''p=str(x).split("'Valid upto': '")
+    t=p[1].split(" Hrs")
+    return(int(t[0]))'''
+data = web_scrapping();
+info = data_cleaning(data);
+store_data(info);'''
+id = 'NICOBAR';
+data=data_query(id);
+print(data)'''
