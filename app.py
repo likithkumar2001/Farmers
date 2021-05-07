93319e4f097e72c024ffca4d4eeee7ae7587ca7a
likithkumar2001 <83726218+likithkumar2001@users.noreply.github.com>
2021-05-07 16:11:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ import numpy as np
 import pandas as pd
 import json
 import urllib
-from pymongo import MongoClient
+import pymongo
 import dns
 from datetime import datetime
 st.title("**HERE YOU GET UPTO DATE INFORMATION ABOUT WEATHER All OVER INDIA.....**")
 uri = 'mongodb+srv://likith:' + urllib.parse.quote(
     "Rp-iA@c6!Nq45c4") + '@cluster0.ms0ap.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-client = MongoClient(uri)
+client = pymongo.MongoClient(uri)
 db = client.weather
 data = db.data
 result = st.selectbox('Type your place here.....',data.distinct( "title"))
