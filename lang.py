2522273fd514aa2fd831e6070dccce914c4cae0e
likithkumar2001 <83726218+likithkumar2001@users.noreply.github.com>
2021-05-09 16:07:50 +0530

    Update lang.py

--- lang.py
+++ lang.py
@@ -1,5 +1,5 @@
 def trans(language,text):
     from googletrans import Translator, constants
     translator = Translator()
-    Text_message = translator.translate(text, dest=language)
+    Text_message = translator.translate(text,src='en',dest=language)
     return Text_message.text
